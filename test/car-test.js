0af2ff39ccc96dae6f642c9d01cf98b772972c6a
Elijah Williams <ejwill04@gmail.com>
2016-11-03 15:48:42 -0600

    Add testing packages, separated car.js, and began testing

---
+++ test/car-test.js
@@ -0,0 +1,53 @@
+var chai = require('chai');
+var assert = chai.assert;
+
+var Car = require('../lib/car');
+
+describe('Car', function() {
+
+  it('should be a function', function () {
+    assert.isFunction(Car);
+  });
+
+  it('should instantiate our good friend, car', function () {
+    var car = new Car();
+    assert.isObject(car);
+  });
+
+  it('should take the first argument and set it as the "x" property of the instantiated object', function () {
+    var car = new Car(15);
+    assert.equal(car.x, 15);
+  });
+
+  it('should take take the second argument and set it as the "y" property of the instantiated object', function () {
+    var car = new Car(15, 30);
+    assert.equal(car.y, 30);
+  });
+
+  it('should take the third argument and set it as the "width" property of the instantiated object', function () {
+    var car = new Car(15, 30, 50);
+    assert.equal(car.width, 50);
+  });
+
+  it('should take the fourth argument and set it as the "height" property of the instantiated object', function () {
+    var car = new Car(15, 30, 50, 80);
+    assert.equal(car.height, 80);
+  });
+
+  it('should take a fifth arguement and set it as the "direction" property of the instantiated object', function () {
+    var car = new Car(15, 30, 50, 80, 'right');
+    assert.equal(car.direction, 'right');
+  });
+
+  it('should take a sixth arguement and set it as the "canvas" property of the instantiated object', function () {
+    var car = new Car(15, 30, 50, 80, 'right');
+    assert.equal(car.canvas);
+  });
+
+  it.skip('"move()" should increment the "x" property by 1', function () {
+    var car = Car(50, 30, 50, 80, 'right');
+    this.move();
+    assert.equal(car.x, 51);
+  });
+
+});
--- test/index.js
+++ test/index.js
@@ -1,12 +1,1 @@
-const assert = require('chai').assert;
-
-describe('our test bundle', function () {
-  it('should work', function () {
-    assert(true);
-  });
-});
-
-it('should to ba function', function () {
-  var blocks = new Block('George');
-  assert.isFunction(block.name, 'George');
-});
+require('./car-test.js');

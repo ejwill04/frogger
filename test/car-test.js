b49801f2aadcce2d78509b3929249dad13e2f9e4
Elijah Williams <ejwill04@gmail.com>
2016-11-05 12:51:56 -0600

    Test car-test

--- test/car-test.js
+++ test/car-test.js
@@ -1,54 +1,60 @@
 var chai = require('chai');
 var assert = chai.assert;
-
 var Car = require('../lib/car');
 
 describe('Car', function() {
+  var world = {context: 300};
+  var car = new Car(25, 15, 50, 25, 'right', world);
 
   it('should be a function', function () {
     assert.isFunction(Car);
   });
 
   it('should instantiate our good friend, car', function () {
-    var canvas = document.getElementById('game');
-    var car = new Car(null, null, null, null, null, canvas);
      assert.isObject(car);
   });
 
   it('should take the first argument and set it as the "x" property of the instantiated object', function () {
-    var car = new Car(15);
-    assert.equal(car.x, 15);
+    assert.equal(car.x, 25);
   });
 
   it('should take take the second argument and set it as the "y" property of the instantiated object', function () {
-    var car = new Car(15, 30);
-    assert.equal(car.y, 30);
+    assert.equal(car.y, 15);
   });
 
   it('should take the third argument and set it as the "width" property of the instantiated object', function () {
-    var car = new Car(15, 30, 50);
     assert.equal(car.width, 50);
   });
 
   it('should take the fourth argument and set it as the "height" property of the instantiated object', function () {
-    var car = new Car(15, 30, 50, 80);
-    assert.equal(car.height, 80);
+    assert.equal(car.height, 25);
   });
 
   it('should take a fifth arguement and set it as the "direction" property of the instantiated object', function () {
-    var car = new Car(15, 30, 50, 80, 'right');
     assert.equal(car.direction, 'right');
   });
 
-  it('should take a sixth arguement and set it as the "canvas" property of the instantiated object', function () {
-    var car = new Car(15, 30, 50, 80, 'right');
-    assert.equal(car.canvas);
+  it('should take a sixth arguement and set it as the "world" property of the instantiated object', function () {
+    assert.equal(car.world, world);
   });
 
-  it.skip('"move()" should increment the "x" property by 1', function () {
-    var car = Car(50, 30, 50, 80, 'right');
-    this.move();
-    assert.equal(car.x, 51);
+  it('should set a property of context to world.context', function (){
+    assert.equal(car.context, 300);
+  });
+
+  it('draw should be a prototype of Car', function () {
+    assert.isFunction(car.draw);
+  });
+
+  it('"car.move()" should increment by 1 if direction is right', function () {
+    car.move();
+    assert.equal(car.x, 26);
+  });
+
+  it('"car.move()" should decrement by 1 if direction is left', function () {
+    car.direction = "left";
+    car.move();
+    assert.equal(car.x, 25);
   });
 
 });

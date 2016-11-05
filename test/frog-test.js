75129bb96d4ad75f9e508cf1bd5d0389da1148c3
Elijah Williams <ejwill04@gmail.com>
2016-11-05 14:45:58 -0600

    Add frog tests

--- test/frog-test.js
+++ test/frog-test.js
@@ -4,51 +4,56 @@ var assert = chai.assert;
 var Frog = require('../lib/frog');
 
 describe('Frog', function() {
+  var world = {context: 300};
+  var frog = new Frog(500, 575, 25, 25, world);
+
 
   it('should be a function', function () {
     assert.isFunction(Frog);
   });
 
   it('should instantiate our good friend, frog', function () {
-    var canvas = document.getElementById('game');
-    var frog = new Frog(null, null, null, null, null, canvas);
      assert.isObject(frog);
   });
 
   it('should take the first argument and set it as the "x" property of the instantiated object', function () {
-    var frog = new Frog(15);
-    assert.equal(frog.x, 15);
+    assert.equal(frog.x, 500);
   });
 
   it('should take take the second argument and set it as the "y" property of the instantiated object', function () {
-    var frog = new Frog(15, 30);
-    assert.equal(frog.y, 30);
+    assert.equal(frog.y, 575);
   });
 
   it('should take the third argument and set it as the "width" property of the instantiated object', function () {
-    var frog = new Frog(15, 30, 50);
-    assert.equal(frog.width, 50);
+    assert.equal(frog.width, 25);
   });
 
   it('should take the fourth argument and set it as the "height" property of the instantiated object', function () {
-    var frog = new Frog(15, 30, 50, 80);
-    assert.equal(frog.height, 80);
+    assert.equal(frog.height, 25);
   });
 
-  it('should take a fifth arguement and set it as the "direction" property of the instantiated object', function () {
-    var frog = new Frog(15, 30, 50, 80, 'right');
-    assert.equal(frog.direction, 'right');
+  it('should take a fifth arguement and set it as the "world" property of the instantiated object', function () {
+    assert.equal(frog.world, world);
   });
 
-  it('should take a sixth arguement and set it as the "canvas" property of the instantiated object', function () {
-    var frog = new Frog(15, 30, 50, 80, 'right');
-    assert.equal(frog.canvas);
+  it('should set a property of context to world.context', function (){
+    assert.equal(frog.context, 300);
   });
 
-  it.skip('"move()" should increment the "x" property by 1', function () {
-    var frog = Frog(50, 30, 50, 80, 'right');
-    this.move();
-    assert.equal(frog.x, 51);
+  it('draw should be a prototype of Car', function () {
+    assert.isFunction(frog.draw);
   });
+
+  it('"frog.move()" should increment by 1 if direction is right', function () {
+    frog.move();
+    var moveKeyStatus = {right: true};
+    document.addEventListener("keydown", true, false);
+    assert.equal(frog.x, 510);
+  });
+
+  // it('"frog.move()" should decrement by 1 if direction is left', function () {
+  //   frog.move();
+  //   assert.equal(frog.x, 500);
+  // });
 
 });
